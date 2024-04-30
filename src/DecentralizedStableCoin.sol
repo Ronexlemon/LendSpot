131aba5a8033c45456dc0b3311839422b54d20d8
RonexLemon <ronexondimu@gmail.com>
2024-04-30 05:13:39 +0300

    erc20

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -1,7 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
+import {ERC20Burnable,ERC20} from '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
+/**
+ * @title  DecentralizedStableCoin
+ * @author Ronex
+ * Colateral: (ETH & BTC)
+ * Relatively stability: Pegged to USD
+ * 
+ */
 
 contract DecentralizedStableCoin{
+    constructor(){
 
-    
+    }
+
+
 }
